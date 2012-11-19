3b0d3bc2f524167fffa58cf2c4ff5623372112e6
ned <iamcalledned@gmail.com>
2012-11-19 16:57:52 -0500

    set adb to be route in a user build

--- core/main.mk
+++ core/main.mk
@@ -264,8 +264,8 @@ user_variant := $(filter user userdebug,$(TARGET_BUILD_VARIANT))
 enable_target_debugging := true
 tags_to_install :=
 ifneq (,$(user_variant))
-  # Target is secure in user builds.
-  ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=1
+  # Target is secure in user builds. NED, changed ro.secure to no
+  ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
 
   ifeq ($(user_variant),userdebug)
     # Pick up some extra useful tools
